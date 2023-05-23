1c28553f06622e11fbfe53023364381f430cec30
Niek van der Plas <niekvanderplas1@gmail.com>
2023-05-23 15:50:09 +0200

    Changed CV

--- src/utils/mockdata.tsx
+++ src/utils/mockdata.tsx
@@ -49,7 +49,7 @@ export const heroData: hero = {
   actions: {
     resume: {
       text: "Resume",
-      url: "/docs/Resume Niek.pdf",
+      url: "/docs/Resume_Niek.pdf",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
